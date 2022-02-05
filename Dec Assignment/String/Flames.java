ec444e7a3c7fab5b3c32ce2b51fd720d709dc509
Abhi <abhishekchauhan9036@gmail.com>
2022-02-05 11:50:59 +0530

    commit message

--- Dec Assignment/String/Flames.java
+++ Dec Assignment/String/Flames.java
@@ -1,57 +1,60 @@
 import java.io.*; // for handling input/output
 import java.util.*; // contains Collections framework
 
-// don't change the name of this class
-// you can add inner classes if needed
 class Main {
 	public static void main (String[] args) {
-                      // Your code here
-	
-		Scanner inputTaker = new Scanner(System.in);
-		String str1 = inputTaker.nextLine();
-		String str2 = inputTaker.nextLine();
+        Scanner sc=new Scanner(System.in);
+		String str1;
+		String str2;
+		str1=sc.next();
+		str2=sc.next();
 
-		int[] countArr1 = new int[26];
-		int[] countArr2 = new int[26];
+		int count1[]=new int[26];
+		int count2[]=new int[26];
 
-		for(int i = 0;i< str1.length(); i++){
-			countArr1[str1.charAt(i) - 'a']++;
+		for(int i=0;i<str1.length();i++){
+			count1[str1.charAt(i)-'a']++;
 		}
 
-		for(int i = 0;i< str2.length(); i++){
-			countArr2[str2.charAt(i) - 'a']++;
+		for(int i=0;i<str2.length();i++){
+			count2[str2.charAt(i)-'a']++;
 		}
 
-		int sum = 0;
-		for(int i =0;i<26;i++){
-			sum+= Math.abs(countArr2[i] - countArr1[i]);
+		int sum=0;
+		for(int i=0;i<26;i++){
+			sum+=Math.abs(count2[i]-count1[i]);
 		}
 
-		sum = sum%6;
+		sum=sum%6;
+// If obtained value is 1, output "Friends"
+// If obtained value is 2, output "Love"
+// If obtained value is 3, output "Affection"
+// If obtained value is 4, output "Marriage"
+// If obtained value is 5, output "Enemy"
+// If obtained value is 0, output "Siblings"
 		switch(sum){
-
-			case 1:{
+			case 1:
 				System.out.println("Friends");
 				break;
-			}
-			case 2:{
+
+			case 2:
 				System.out.println("Love");
 				break;
-			}
-			case 3:{
+
+			case 3:
 				System.out.println("Affection");
 				break;
-			}
-			case 4:{
-				System.out.println("Marriage");
-				break;
-			}
-			case 5:{
+
+			case 4:
+			 	System.out.println("Marriage");
+				 break;
+				
+			case 5:
 				System.out.println("Enemy");
 				break;
-			}
-			default: System.out.println("Siblings");
+
+			case 0:
+				System.out.println("Siblings");
 		}
-		
 	}
 }
